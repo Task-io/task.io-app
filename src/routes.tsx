8957b84f5ddb3ea3b56fc54e01294dcfb603b54a
Pedro Henrique Karnoski <pedrokarnoski@gmail.com>
2024-06-09 21:57:45 -0300

    feat: Página de Error e 404

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,14 +2,17 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from '@/pages/_layouts/app'
 import { AuthLayout } from '@/pages/_layouts/auth'
+import { NotFound } from '@/pages/404'
 import { ListTasks } from '@/pages/app/tasks/list-tasks'
 import { SignIn } from '@/pages/auth/sign-in'
 import { SignUp } from '@/pages/auth/sign-up'
+import { Error } from '@/pages/error'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [{ path: '/', element: <ListTasks /> }],
   },
   {
@@ -25,5 +28,9 @@ export const router = createBrowserRouter([
         element: <SignUp />,
       },
     ],
+  },
+  {
+    path: '*',
+    element: <NotFound />,
   },
 ])
