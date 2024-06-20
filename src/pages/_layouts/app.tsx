1837c0e316522c248bac1182230108bda711b2cc
Pedro Henrique Karnoski <pedrokarnoski@gmail.com>
2024-06-19 21:45:46 -0300

    chore: Remover logs de depuração

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -15,12 +15,8 @@ export function AppLayout() {
         if (isAxiosError(error)) {
           const status = error.response?.status
           const url = error.response?.config?.url
-          console.log('Erro interceptado:', error) // Log do erro para depuração
-          console.log('Status:', status) // Log do status para depuração
-          console.log('URL:', url) // Log da URL para depuração
 
           if (status === 401) {
-            console.log('Redirecionando para /sign-in') // Log para confirmar o redirecionamento
             navigate('/sign-in', { replace: true })
           } else {
             throw error
